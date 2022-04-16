9de050458fcd192f1a053e22cde405cb57c75f2f
weigand (Mr. ) <weigand.stephen@gmail.com>
2022-04-16 20:57:49 +0000

    Version 0.0.2 allows multiple variables on the left hand side
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/twrap@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yat/R/yat.R
+++ pkg/yat/R/yat.R
@@ -9,11 +9,37 @@ formsplit <- function(x) {
     lapply(rhss, function(x) formula(paste(lhs, x, sep = " ~ ")))
 }
 
+## This is like formsplit1 but handles multiple lhs variables and the
+## result is a matrix whose elements are a list whose first element is
+## a formula. So `vs + am ~ mean(mpg) + mean(hp)` becomes
+##
+##      [,1]           [,2]          
+## [1,] vs ~ mean(mpg) am ~ mean(mpg)
+## [2,] vs ~ mean(hp)  am ~ mean(hp) 
+##
+## To get this to be 1-dimensional I think one sets the `dim` attribute
+## to NULL.
+
+formsplit <- function(x) {
+    char <- as.character(x)
+    lhss <- strsplit(char[2], "+", fixed = TRUE)
+    rhss <- strsplit(char[3], "+", fixed = TRUE)
+    outr <- outer(lhss[[1]], rhss[[1]], FUN = paste, sep = " ~ ")
+    apply(t(outr), 1:2, formula) # Gives a matrix
+}
+
 ## Take a formula of the form y ~ f(x) and do f(x) for each value of y
+## Can version 2 handle formulas with a constant 1 ~ mean(mpg)?
 rhs.by.lhs <- function(formula, data) {
     char <- as.character(formula)
-    ## split by lhs
-    groups <- split(data, formula(paste(char[1], char[2])))
+    newrhs <- formula(paste(char[1], char[2]))
+    groups <-
+        if (identical(newrhs, ~ 1)) {
+            list(Total = data)
+        } else {
+            ## split by lhs. Uses split method for data.frame
+            split(data, formula(paste(char[1], char[2])))
+        }
     ## apply by rhs
     results <- lapply(groups,
                       model.frame,
@@ -27,7 +53,7 @@ rhs.by.lhs <- function(formula, data) {
     if (is.null(cmt)) {
         df$variable <- sapply(results, names)[1:nrow(df)]
         df$categories <- NA_character_
-    } else{
+    } else {
         df$variable <- cmt[1L]
         df$categories <- if (length(cmt) == 1L) NA_character_ else cmt[-1L]
     }
@@ -36,14 +62,56 @@ rhs.by.lhs <- function(formula, data) {
            setdiff(names(df), c("variable", "categories")))]
 }
 
+## Take a list `x` of data frames and how many rows
+## and do something along the lines of
+## dataframe(x, nrows = 2)
+##
+## [[1]]
+##    variable categories        0        1
+## 1 mean(mpg)       <NA> 16.61667 24.55714
+##
+## [[2]]
+##   variable categories        0        1
+## 1 mean(hp)       <NA> 189.7222 91.35714
+##
+## [[3]]
+##    variable categories        0        1     2
+## 1 mean(mpg)       <NA> 17.14737 24.39231  22.5
+##
+## [[4]]
+##   variable categories        0        1      2
+## 1 mean(hp)       <NA> 160.2632 126.8462   88.1
+arrange.results <- function(x, nrows, ncols) {
+    ## data frame we get if there is only one
+    ## lhs variable
+    out <- do.call(rbind, x[1:nrows])
+
+    ## Now merge in repeatedly other rhs variables (if any)
+    if (ncols > 1) {
+        ## indices of ultimate arrangement
+        indices <- matrix(1:(nrows * ncols), nrow = nrows, byrow = FALSE)
+        
+        for (i in 2:ncols) {
+            out.next <- do.call(rbind, x[indices[,i]])
+            out <- merge(out,
+                         out.next,
+                         by = c("variable", "categories"))
+        }
+    }
+    out
+}
+
 ## yat stands for yet another table
 yat <- function(formula, data) {
     forms <- formsplit(formula)
+    dims <- dim(forms)
+    dim(forms) <- NULL
     smry <- lapply(forms, rhs.by.lhs, data = data)
-    do.call(rbind, smry)
+    arrange.results(smry, dims[1], dims[2])
 }
 
-## Summary functions that format their output
+
+## One of a few summary functions that format their output
 mediqr <- function(x, lab = NULL, dig = 0) {
     smry <- quantile(x, c(0.5, 0.25, 0.75), na.rm = TRUE)
     out <- sprintf("%s (%s, %s)",
